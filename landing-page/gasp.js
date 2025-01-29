0351abf12456065b8e9350626a6c3bb9d0b5bd69
samdo817 <phantom72008@gmail.com>
2025-01-29 21:49:13 +0700

    Second Commit

---
+++ landing-page/gasp.js
@@ -0,0 +1,61 @@
+window.addEventListener("load", function() {
+    gsap.from(".logo", { 
+      duration: 1, 
+      opacity: 0, 
+      y: -100
+    });
+    
+    gsap.from(".nav-container", { 
+      duration: 1, 
+      opacity: 0, 
+      y: -100,
+      delay: 0.4
+    });
+
+    gsap.from(".nav-right", {
+        duration: 1,
+        opacity: 0,
+        y: -100,
+        delay: 0.8
+    });
+
+    gsap.from(".tagline h3", {
+        duration: 0.5,
+        opacity: 0,
+        scale: 0,
+        ease: "power2.out",
+    });
+
+    gsap.from(".tagline p", {
+        duration: 0.5,
+        opacity: 0,
+        scale: 0,
+        delay: 0.2,
+        ease: "power2.out"
+    });
+
+    gsap.from(".card-scroll",{
+        duration: 0.5,
+        opacity: 0,
+        scale: 0,
+        x: -200,
+        delay: 0.2,
+        ease: "power2.out"
+    });
+
+    gsap.from(".btn-destination", {
+        duration: 0.5,
+        opacity: 0,
+        ease: "power2.out",
+        x: -200,
+        delay: 0.8
+    });
+    gsap.from(".card-destination-content", {
+        duration: 0.5,
+        opacity: 0,
+        ease: "power2.out",
+        y: -200,
+        delay: 0.8
+    });
+  });
+  
---
+++ landing-page/main.js
@@ -0,0 +1,89 @@
+// search menu
+const searchIcon = document.querySelector('.search-icon');
+const navContainer = document.querySelector('.nav-container');
+
+searchIcon.addEventListener('click', () => {
+  searchIcon.classList.toggle('active');
+  navContainer.classList.toggle('active');
+});
+
+const guestInput = document.getElementById("guest");
+
+guestInput.addEventListener("keypress", (e) => {
+  if (!/^\d$/.test(e.key)) {
+    e.preventDefault();
+  }
+});
+
+guestInput.addEventListener("input", () => {
+  guestInput.value = guestInput.value.replace(/[^\d]/g, "");
+});
+
+
+// Bookmark destination card
+  const bookmarkIcons = document.querySelectorAll('.bx-bookmark, .bxs-bookmark');
+
+  bookmarkIcons.forEach(icon => {
+    icon.addEventListener('click', () => {
+      if (icon.classList.contains('bx-bookmark')) {
+        icon.classList.replace('bx-bookmark', 'bxs-bookmark');
+        icon.classList.add('active');
+      } else {
+        icon.classList.replace('bxs-bookmark', 'bx-bookmark');
+        icon.classList.remove('active');
+      }
+    });
+  });
+
+  // Caraousel prev & next
+  const scrollContainers = document.querySelectorAll(".card-scroll, .destination-scroll, .promo-scroll");
+  const scrollStep = 240;
+  
+  scrollContainers.forEach((scrollContainer) => {
+    const prevButton = scrollContainer.parentElement.querySelector(".scroll-button.prev");
+    const nextButton = scrollContainer.parentElement.querySelector(".scroll-button.next");
+  
+    prevButton.addEventListener("click", () => {
+      scrollContainer.scrollLeft -= scrollStep;
+    });
+  
+    nextButton.addEventListener("click", () => {
+      scrollContainer.scrollLeft += scrollStep;
+    });
+  
+    scrollContainer.addEventListener("scroll", () => {
+      prevButton.disabled = scrollContainer.scrollLeft === 0;
+      nextButton.disabled =
+        scrollContainer.scrollLeft + scrollContainer.offsetWidth >=
+        scrollContainer.scrollWidth;
+    });
+  
+    prevButton.disabled = scrollContainer.scrollLeft === 0;
+    nextButton.disabled =
+      scrollContainer.scrollLeft + scrollContainer.offsetWidth >= scrollContainer.scrollWidth;
+  });
+
+  // sponsored grid
+  document.querySelector('.next-partner').addEventListener('click', function() {
+    const grids = document.querySelectorAll('.partners-grid, .partners-grid-2, .partners-grid-3');
+    const chevron = document.querySelector('.next-partner i');
+    let visibleCount = 0;
+  
+    grids.forEach(grid => {
+      if (grid.style.display === 'flex') {
+        visibleCount++;
+      }
+    });
+  
+    if (visibleCount === 0) {
+      grids[0].style.display = 'flex';
+    } else if (visibleCount === 1) {
+      grids[1].style.display = 'flex';
+    } else if (visibleCount === 2) {
+      grids[2].style.display = 'flex';
+      chevron.style.transform = 'rotate(180deg)';
+    } else {
+      grids.forEach(grid => grid.style.display = 'none');
+      chevron.style.transform = 'rotate(0deg)';
+    }
+  });
